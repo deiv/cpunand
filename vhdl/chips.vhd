0b07ba4126fa47bc7d40716b5ac3a66f88acf273
David Suárez <david.sephirot@gmail.com>
2018-11-04 02:07:50 +0100

    Add automated tests battery for vhdl chip files

--- vhdl/chips.vhd
+++ vhdl/chips.vhd
@@ -6,7 +6,7 @@ entity and_chip is
 
 	port 
 	(
-		x		: in std_logic;
+		x	: in std_logic;
 		y	: in std_logic;
 		o	: out std_logic
 	);
@@ -19,5 +19,28 @@ begin
 	process (x, y)
 	begin
 		o <= x and y;
+	end process;
+end rtl;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity nand_chip is
+
+	port 
+	(
+		x		: in std_logic;
+		y	: in std_logic;
+		o	: out std_logic
+	);
+
+end entity;
+
+architecture rtl of nand_chip is
+begin
+
+	process (x, y)
+	begin
+		o <= x nand y;
 	end process;
 end rtl;
